90991b0e7cc724cf97c2f88eafd97feafb0de075
Dima Fitiskin <dfitiskin@gmail.com>
2017-03-21 14:11:19 +0100

    Шаг шестой: сбиваем шарики

--- game.js
+++ game.js
@@ -22,6 +22,7 @@ function hideButton() {
 }
 
 function showBubble(line) {
+  line.classList.remove('boom');
   line.classList.add('up');
 }
 
@@ -29,17 +30,34 @@ function hideBubble(line) {
   line.classList.remove('up');
 }
 
+function boomBubble(line) {
+  line.classList.add('boom');
+}
+
 function nextBubble() {
   const bubble = getRandomBubble();
   showBubble(bubble);
-  setTimeout(() => {
+  bubble.timeout = setTimeout(() => {
     hideBubble(bubble);
     nextBubble();
   }, 2000);
 }
 
+function handleBubbleClick() {
+  const bubble = this.parentElement;
+  clearTimeout(bubble.timeout);
+  boomBubble(bubble);
+  hideBubble(bubble);
+  nextBubble();
+}
+
 const lines = document.getElementsByClassName('hole');
+const bubbles = document.getElementsByClassName('bubble');
 const startButton = document.querySelector('.startButton');
+
+for (let bubble of bubbles) {
+  bubble.addEventListener('click', handleBubbleClick);
+}
 
 startButton.addEventListener('click', () => {
   hideButton();
