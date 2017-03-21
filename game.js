66f60e6239dbacbb6a2b2ed40fddfbc960dfc8e7
Dima Fitiskin <dfitiskin@gmail.com>
2017-03-21 12:24:08 +0100

    Шаг первый: скрываем кнопку при клике и показываем шарик

--- game.js
+++ game.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('Скрипт подключен и работает');
+function showButton() {
+  startButton.style.display = 'initial';
+}
+
+function hideButton() {
+  startButton.style.display = 'none';
+}
+
+function showBubble(line) {
+  line.classList.add('up');
+}
+
+function hideBubble(line) {
+  line.classList.remove('up');
+}
+
+const lines = document.getElementsByClassName('hole');
+const startButton = document.querySelector('.startButton');
+
+startButton.addEventListener('click', () => {
+  hideButton();
+  showBubble(lines[3]);
+});
