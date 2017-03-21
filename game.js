d9d79babf37e69da0737574b0ed4176d66437a75
Dima Fitiskin <dfitiskin@gmail.com>
2017-03-21 17:55:03 +0100

    Шаг одиннадцатый: показываем таймер

--- game.js
+++ game.js
@@ -82,12 +82,34 @@ function resetPoints() {
   updateScoreboard();
 }
 
+function startTimer() {
+  showTimer(GAME_TIMEOUT);
+  startedAt = Date.now();
+  startTimer.interval = setInterval(updateTimer, 500);
+}
+
+function updateTimer() {
+  if (!isGameStarted) {
+    return;
+  }
+  let timeout = GAME_TIMEOUT - (Date.now() - startedAt);
+  if (timeout < 0 ) {
+    timeout = 0;
+  }
+  showTimer(timeout);
+}
+
+function showTimer(timeout) {
+  timer.innerHTML = timeToString(timeout);
+}
+
 function startGame() {
   resetPoints();
   isGameStarted = true;
   hideButton();
   nextBubble();
-  setTimeout(stopGame, GAME_TIMEOUT)
+  setTimeout(stopGame, GAME_TIMEOUT);
+  startTimer();
 }
 
 function resetGame() {
@@ -97,6 +119,7 @@ function resetGame() {
 
 function stopGame() {
   isGameStarted = false;
+  clearInterval(startTimer.interval);
 }
 
 function loadTopScore() {
@@ -110,13 +133,27 @@ function saveTopScore(score) {
   localStorage.setItem('topScore', score);
 }
 
+function timeToString(time) {
+  const MSECONDS_IN_SEC = 1000;
+  const MSECONDS_IN_MIN = 60 * MSECONDS_IN_SEC;
+
+  let min = Math.floor(time / MSECONDS_IN_MIN);
+  let sec = Math.floor((time % MSECONDS_IN_MIN) / MSECONDS_IN_SEC);
+  let msec = (time % MSECONDS_IN_MIN) % MSECONDS_IN_SEC;
+  let spacer = msec > 500 ? ':' : '&nbsp;';
+  return [min, sec]
+    .map(number => number >= 10 ? number : `0${number}`)
+    .join(spacer);
+}
+
 const GAME_TIMEOUT = 15000;
-let currentPoints = 0, topPoints = 0, isGameStarted = false;
+let currentPoints = 0, topPoints = 0, isGameStarted = false, startedAt;
 const lines = document.getElementsByClassName('hole');
 const bubbles = document.getElementsByClassName('bubble');
 const startButton = document.querySelector('.startButton');
 const scoreboard = document.getElementById('currentScoreView');
 const bestScore = document.getElementById('topScoreView');
+const timer = document.querySelector('.timer');
 
 for (let bubble of bubbles) {
   bubble.addEventListener('click', handleBubbleClick);
