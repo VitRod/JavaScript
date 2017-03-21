356b437e5e04c0c08462bc1f9822f0b4a0466b36
Dima Fitiskin <dfitiskin@gmail.com>
2017-03-21 12:42:34 +0100

    Шаг четвертый: показываем следующий шарик после скрытия предыдущего

--- game.js
+++ game.js
@@ -4,6 +4,10 @@ function rand(from, to) {
   return Math.floor((to - from + 1) * Math.random()) + from;
 }
 
+function getRandomBubble() {
+  return lines[rand(0, lines.length - 1)];
+}
+
 function showButton() {
   startButton.style.display = 'initial';
 }
@@ -20,15 +24,19 @@ function hideBubble(line) {
   line.classList.remove('up');
 }
 
+function nextBubble() {
+  const bubble = getRandomBubble();
+  showBubble(bubble);
+  setTimeout(() => {
+    hideBubble(bubble);
+    nextBubble();
+  }, 2000);
+}
+
 const lines = document.getElementsByClassName('hole');
 const startButton = document.querySelector('.startButton');
 
 startButton.addEventListener('click', () => {
   hideButton();
-  const bubble = lines[rand(0, lines.length - 1)];
-  showBubble(bubble);
-  setTimeout(() => {
-    hideBubble(bubble);
-    showButton();
-  }, 2000);
+  nextBubble();
 });
